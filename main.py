76665d57c74c668b20cb3ef8c666e9cd378eb4aa
hiepnt <hiepnt@beetinnovators.com>
2024-10-16 16:08:59 +0700

    Add webhook

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 # import url
 from api.url import router as api_url
+from webhooks.webhook import webhook_router
 
 app = FastAPI()
 app.include_router(api_url)
-
-
+app.include_router(webhook_router)
