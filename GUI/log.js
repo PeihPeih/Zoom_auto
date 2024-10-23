325b7ed93ef4809a96cd77527d2aee0a7b8640cd
hiepnt <hiepnt@beetinnovators.com>
2024-10-23 21:08:39 +0700

    Add socket

--- GUI/log.js
+++ GUI/log.js
@@ -1,2 +1,9 @@
 const socket = io('https://zoom-auto.onrender.com/ws');
 
+socket.on('connect', ()=>{
+    console.log('Connected to server');
+})
+
+socket.on('meeting_participant', (data)=>{
+    console.log('Participant info:', data)
+})
