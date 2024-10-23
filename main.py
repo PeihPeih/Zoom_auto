a03465ae5b09abdee54175f1915042f73b001ae0
hiepnt <hiepnt@beetinnovators.com>
2024-10-23 21:23:38 +0700

    Add socket

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 # import url
 from api.url import router as api_url
-from webhooks.webhook import webhook_router, sio
+from webhooks.webhook import webhook_router, sio, sio_app
 import socketio
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -15,7 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-sio_app = socketio.ASGIApp(sio, app)
 
 app.mount("/ws", app=sio_app)
 app.add_route("/socket.io", sio_app, methods=["GET", "POST"])
--- webhooks/webhook.py
+++ webhooks/webhook.py
@@ -13,7 +13,8 @@ webhook_router = APIRouter()
 
 ZOOM_SECRET_TOKEN = os.environ.get("f")
 
-sio = socketio.AsyncServer(async_mode='asgi')
+sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode='asgi')
+sio_app = socketio.ASGIApp(sio)
 
 @webhook_router.post("/webhook")
 async def webhook(request: Request):
