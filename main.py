ff9f51116428dc482fa6a2cf03b324c29cb17dc7
hiepnt <hiepnt@beetinnovators.com>
2024-10-24 07:00:19 +0700

    Add socket

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 # import url
 from api.url import router as api_url
 from webhooks.webhook import webhook_router
 import socketio
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import hmac
+import hashlib
 
 app = FastAPI()
 
@@ -24,4 +27,41 @@ app.add_route("/socket.io", sio_app, methods=["GET", "POST"])
 app.add_api_websocket_route("/socket.io", sio_app)
 
 app.include_router(api_url)
-app.include_router(webhook_router)
+
+ZOOM_SECRET_TOKEN = os.environ.get("f")
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    print(ZOOM_SECRET_TOKEN)
+    headers = dict(request.headers)
+    body = await request.json()
+    print(headers)
+    print(body)
+
+    if 'payload' in body and 'plainToken' in body['payload']: # Dùng để validated url trên link zoom webhook
+        secret_token = ZOOM_SECRET_TOKEN.encode("utf-8")
+        plaintoken = body['payload']['plainToken']
+        mess = plaintoken.encode("utf-8")
+        has = hmac.new(secret_token, mess, hashlib.sha256).digest()
+        hexmessage = has.hex()
+
+        response = {
+            'message': {
+                'plainToken': plaintoken,
+                'encryptedToken': hexmessage
+            }
+        }
+        return response['message']
+    
+    event = body['event']
+    payload = body['payload']
+
+    # Event started meeting        
+    if event == 'meeting.participant_joined':
+        data = {}
+        object = payload['object']
+        participant = object['participant']
+        data['name']=participant['user_name']
+        data['join_time']=participant['join_time']
+        await sio.emit("participant_joined", data)
+        return "Đã gửi data qua socket"
--- webhooks/webhook.py
+++ webhooks/webhook.py
@@ -1,50 +1,13 @@
-from fastapi import APIRouter, Request, HTTPException, Header
-import json
-import secrets
-import hmac
-import hashlib
-import os
-from dotenv import load_dotenv
-from main import sio
+# from fastapi import APIRouter, Request, HTTPException, Header
+# import json
+# import secrets
+# import hmac
+# import hashlib
+# import os
+# from dotenv import load_dotenv
+# from main import sio
 
-load_dotenv()
+# load_dotenv()
 
-webhook_router = APIRouter()
+# webhook_router = APIRouter()
 
-ZOOM_SECRET_TOKEN = os.environ.get("f")
-
-@webhook_router.post("/webhook")
-async def webhook(request: Request):
-    print(ZOOM_SECRET_TOKEN)
-    headers = dict(request.headers)
-    body = await request.json()
-    print(headers)
-    print(body)
-
-    if 'payload' in body and 'plainToken' in body['payload']: # Dùng để validated url trên link zoom webhook
-        secret_token = ZOOM_SECRET_TOKEN.encode("utf-8")
-        plaintoken = body['payload']['plainToken']
-        mess = plaintoken.encode("utf-8")
-        has = hmac.new(secret_token, mess, hashlib.sha256).digest()
-        hexmessage = has.hex()
-
-        response = {
-            'message': {
-                'plainToken': plaintoken,
-                'encryptedToken': hexmessage
-            }
-        }
-        return response['message']
-    
-    event = body['event']
-    payload = body['payload']
-
-    # Event started meeting        
-    if event == 'meeting.participant_joined':
-        data = {}
-        object = payload['object']
-        participant = object['participant']
-        data['name']=participant['user_name']
-        data['join_time']=participant['join_time']
-        await sio.emit("participant_joined", data)
-        return "Đã gửi data qua socket"
