c80e1809c3068c368e1336b9334561b3ce02ab94
hiepnt <hiepnt@beetinnovators.com>
2024-10-24 07:54:29 +0700

    Add socket

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, Request
 from api.url import router as api_url
 # from webhooks.webhook import webhook_router
 import socketio
-from fastapi.middleware.cors import CORSMiddleware
 import os
 import hmac
 import hashlib
@@ -15,7 +14,7 @@ app = FastAPI()
 
 # Tạo socketio server và kết nối với FastAPI
 sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode='asgi')
-sio_app = socketio.ASGIApp(sio, app)
+sio_app = socketio.ASGIApp(sio)
 
 app.mount("/ws", sio_app)
 app.add_route("/socket.io", sio_app, methods=["GET", "POST"])
